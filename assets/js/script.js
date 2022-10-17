5651fd3d3394f4d16975920b9f9d7af1c05702ae
AmineeeEl <amineelaabdi@gmail.com>
2022-10-17 19:35:07 +0100

    Started working on the DeleteTask

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,58 @@
+
 // __________________________________________________________________________variablesNeededAsGlobal
 var howManyTasks=0;
+var toDelete=0;
 
 // __________________________________________________________________________ADD A TASK
 function add()
 {   
     if(document.getElementsByClassName('inputChecker')[0].value.trim()!=0)
     {   
-        document.getElementById('save').disabled = false;
+        let Temp =[{
+        title         :   '',
+        type          :   '',
+        priority      :   '',
+        deadLine      :   '',
+        creation      :   '',
+        description   :   ``}];
         howManyTasks++;
-        let title = document.getElementById('title').value;
-        title= title.charAt(0).toUpperCase() + title.toLowerCase().slice(1);
-        let type;
+        Temp.title = document.getElementById('title').value;
+        Temp.title = Temp.title .charAt(0).toUpperCase() + Temp.title .toLowerCase().slice(1);
+        
         if (document.getElementById('feature').checked){
-            type=document.getElementById('feature').value;
+            Temp.type=document.getElementById('feature').value;
         }
         else if (document.getElementById('bug').checked){
-            type=document.getElementById('bug').value;
+            Temp.type=document.getElementById('bug').value;
         }
-        let priority =document.getElementById('priority').value;
-        let description =document.getElementById("Description").value;
+        Temp.priority =document.getElementById('priority').value;
+        Temp.description =document.getElementById("Description").value;
         let shortDescription;
-        if(description.length>30)
+        if(Temp.description.length>30)
         {
-            shortDescription = description.slice(0,30)+"...";
+            shortDescription = Temp.description.slice(0,30)+"...";
         }
         else
         {
-            shortDescription = description;
+            shortDescription = Temp.description;
         } 
-        let Deadline;
-        Deadline =document.getElementById('date').value;
-        let creationDate = Date().slice(0,21);
-        let button = `  <button id="${"Task"+howManyTasks}" class="list-group-item-action mx-0 border row bg-white align-items-center  pb-4px" onclick="fullVIew()" data-bs-toggle="modal" data-bs-target="#fullView${howManyTasks}">
+        
+        Temp.deadLine =document.getElementById('date').value;
+        Temp.creation = Date().slice(0,21);
+        tasks[howManyTasks]= Temp;
+        let button = ` <button id="${"Task"+howManyTasks}" class="list-group-item-action mx-0 border row bg-white align-items-center  pb-4px" data-bs-toggle="modal" data-bs-target="#fullView${howManyTasks}" >
                             <div class="col-1">
                                 <i class="bi bi-question-square text-green fs-18px"></i> 
                             </div>
                             <div class="col-11">
-                                <div class="text-dark fw-bolder fs-5 ">${title}</div>
+                                <div class="text-dark fw-bolder fs-5 ">${tasks[howManyTasks].title}</div>
                                 <div class="">
-                                    <div class="text-muted fs-5">#${howManyTasks} Created in ${creationDate}</div>
+                                    <div class="text-muted fs-5">#${howManyTasks} Created in ${tasks[howManyTasks].creation}</div>
                                     <div class="mt-2 fs-5">${shortDescription}</div>
                                 </div>
                                 <div class="mt-1">
-                                    <span class="bg-primary fs-13px text-white fw-bold" style="letter-spacing:1.3px;border-radius:8px; padding: 2px 4.5px 2px 5px;">${priority}</span>
-                                    <span class=" fs-13px text-dark fw-bold"style="letter-spacing:1.3px;  background-color: rgb(202, 202, 202); border-radius:8px; padding: 2px 4.5px 2px 5px;">${type}</span>
+                                    <span class="bg-primary fs-13px text-white fw-bold" style="letter-spacing:1.3px;border-radius:8px; padding: 2px 4.5px 2px 5px;">${tasks[howManyTasks].priority}</span>
+                                    <span class=" fs-13px text-dark fw-bold"style="letter-spacing:1.3px;  background-color: rgb(202, 202, 202); border-radius:8px; padding: 2px 4.5px 2px 5px;">${tasks[howManyTasks].type}</span>
                                 </div>
                             </div>
                         </button>
@@ -52,42 +61,48 @@ function add()
 			                <div class="modal-content">
 
 			                    <div class="modal-header">
-				                    <div class="h2">${title} (${type})</div>
+				                    <div class="h2">${tasks[howManyTasks].title} (${tasks[howManyTasks].type})</div>
 			                    </div>
 
 			                    <div class="modal-body">
                                     <div class="row">
                                         <h4 class="text-dark col-3">Created on:</h4>
-                                        <span class="text-green fs-15px col-9">${creationDate}</span>
+                                        <span class="text-muted fs-15px col-9">${tasks[howManyTasks].creation}</span>
                                     </div>                             
                                     
                                     <div class="row mt-15px">
                                         <h4 class="text-dark col-3"">Deadline:</h4>
-                                        <span class="text-danger fs-15px col-9">${Deadline}</span>
+                                        <span class="text-danger fs-15px col-9">${tasks[howManyTasks].deadLine}</span>
                                     </div>
                                     <div class="row mt-15px">
                                         <h4 class="text-dark col-3"">Priority:</h4>
-                                        <span class="text-muted fs-15px col-9">${priority}</span>
+                                        <span class="text-muted fs-15px col-9">${tasks[howManyTasks].priority}</span>
                                     </div>
                                     <div class="row mt-15px ">		
                                         <h4 class="text-dark col-3"">Description:</h4>
-                                        <span class="text-muted fs-15px col-9" style="width:400px;overflow:hidden;">${description}</span>
+                                        <span class="text-muted fs-15px col-9" style="width:400px;overflow:hidden;">${tasks[howManyTasks].description}</span>
                                     </div>
-                                </div>
-			  
-			                </div>
-		                </div>
-	                </div>`;               
+                                    <div class="modal-footer">
+                                    <button id="Delete${howManyTasks}" onclick="deleteTask(this.id)" type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+                                    </div>
+	                            </div>
+                            </div>
+			            </div>
+		                </div>`;               
         toDoCard.innerHTML+=button;
         document.getElementById('title').value="";
         document.getElementById('Description').value="";
         $('#modal-task').modal('hide')
     }
+    console.log(tasks);
 }
 
 
-// __________________________________________________________________________fullView A TASK
-function fullView()
-{
-   
-}  
+// __________________________________________________________________________DELET A TASK
+function deleteTask(ID)
+{   
+    let idToDelete=ID.slice(-1);
+    tasks[idToDelete]=tasks[idToDelete+1];
+    delete tasks[tasks.length];
+    console.log(tasks);
+}
